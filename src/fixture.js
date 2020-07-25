6f791cbb2b00160746978382294b4a3a3838dd6c
masbossun <setiagi.ryan@gmail.com>
2020-07-25 14:54:24 +0700

    fix grammar issue

--- src/fixture.js
+++ src/fixture.js
@@ -5,7 +5,7 @@ const data = {
   headline: "as a software\ndeveloper, ryan\ndoes his works\nwith passion",
   about: [
     "his name is Ryan Setiagi, he currently working as a fulltime mobile developer at [rukita](https://rukita.co). basically he does frontend development for web and also for mobile app. he quite familiar with react environment such as [reactjs](https://reactjs.org/) and [react native](https://reactnative.dev). not tied to exact same development or environment, he can explore another frameworks and works with it if needed.",
-    "he have been falling in love with programming since he was at college, and coding is how he express his feelings. In the mean time, he wrote some stuff on his [blog](/blog/). Just to keep his memory sharp, in case he need it in the future. he have a great interests to learning and building something new. If you intersted, feel free to [contact him](#contacts).",
+    "he has been falling in love with programming since he was at college, and coding is how he express his feelings. In the mean time, he wrote some stuff on his [blog](/blog/). Just to keep his memory sharp, in case he need it in the future. he has a great interests to learning and building something new. If you intersted, feel free to [contact him](#contacts).",
   ],
   projects: [
     {
